b0300c61a02d018a4397db2b252857b639da08f2
Stephen Poole <Stephen.Poole@redlioncanada.com>
2015-10-13 14:34:06 -0400

    Updated deploy gulp file for kitchenaid

--- deploy/gulpfile.js
+++ deploy/gulpfile.js
@@ -15,10 +15,10 @@ var ftp = require('gulp-sftp');
 var replace = require('gulp-replace');
 var p = require('../package.json');
 
-var basePath = '/home/wpcstage/mymaytag/';
-var versionPath = '/home/wpcstage/mymaytag/latest/';
+var basePath = '/home/wpcstage/mykitchenaid/';
+var versionPath = '/home/wpcstage/mykitchenaid/latest/';
 var opts = {host: 'wpc-stage.com', port: 22, auth: 'keyMain'};
-var baseURL = 'http://mymaytag.wpc-stage.com';
+var baseURL = 'http://mykitchenaid.wpc-stage.com';
 
 gulp.task('default', ['version'], function() {
     doUpload(['config', 'css', 'fonts', 'js', 'views']);
