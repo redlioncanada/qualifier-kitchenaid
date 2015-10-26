72683915d45260d729859ed5eb73c57ac15dec3d
Stephen Poole <Stephen.Poole@redlioncanada.com>
2015-10-26 14:06:17 -0400

    Fixed being unable to navigate past the first question on Cooktops, and Ranges/Wall Ovens/Vents&Hoods having 0 results

--- app/js/rank.js
+++ app/js/rank.js
@@ -5,17 +5,15 @@ angular.module('App')
 	$scope.sortable = {}
     $scope.initialize = function (qs) {
         for (var i in qs.text[0].answers) {
-            console.log(qs.text[0].answers[i].answer);
             if (!isNaN(qs.text[0].answers[i].answer)) {
                 qs.text[0].answers[i].order = qs.text[0].answers[i].answer
             }
-            console.log(qs.text[0].answers[i].order);           
+            // console.log(qs.text[0].answers[i].order);           
         }
     }
 
 	$scope.sortable.dragControlListeners = {
     	orderChanged: function(event) {
-            console.log(event);
             $rootScope.controls.questionHasAnswer = true
     		for (var i in $rootScope.questionsData.question.show.answers) {
     			$rootScope.questionsData.question.show.answers[i].answer = i
