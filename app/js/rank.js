c223f706d40cc0ddc180c37018801a43ce799569
Stephen Poole <Stephen.Poole@redlioncanada.com>
2015-10-27 18:02:25 -0400

    Added a drag animation to rank question. Fixed dish q5 image bathing.

--- app/js/rank.js
+++ app/js/rank.js
@@ -3,6 +3,8 @@
 angular.module('App')
   .controller('RankCtrl', function ($scope, $rootScope) {
 	$scope.sortable = {}
+    $scope.clonedElement;
+
     $scope.initialize = function (qs) {
         for (var i in qs.text[0].answers) {
             if (!isNaN(qs.text[0].answers[i].answer)) {
@@ -12,6 +14,19 @@ angular.module('App')
         }
     }
 
+    $scope.draggingListener = function(e) {
+        console.log('movemove');
+        if (!$($scope.clonedElement)[0].parentElement) {
+            console.log('append');
+            $scope.clonedElement.appendTo('body').show();
+        }
+        $($scope.clonedElement).css({
+            position: 'absolute',
+            top: e.pageY,
+            left: e.pageX
+        })
+    };
+
 	$scope.sortable.dragControlListeners = {
     	orderChanged: function(event) {
             $rootScope.controls.questionHasAnswer = true
@@ -19,6 +34,20 @@ angular.module('App')
     			$rootScope.questionsData.question.show.answers[i].answer = i
     		}
     	},
+        dragStart: function(e) {
+            console.log(e);
+           $scope.clonedElement = $(e.source.itemScope.element[0]).clone()
+                .addClass('dragging-rank')
+                .removeClass('as-sortable-item')
+                .attr('as-sortable-item', '');
+           console.log($scope.clonedElement);
+           $(document).on('mousemove', $scope.draggingListener);
+        },
+        dragEnd: function(e) {
+            $($scope.clonedElement).remove();
+            $scope.clonedElement = undefined;
+            $(document).off('mousemove', $scope.draggingListener);
+        },
 	    containment: '.answers-main-content'
 	};
 });
