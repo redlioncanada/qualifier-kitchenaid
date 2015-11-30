d4faee8455692e0af2697f01fcfde1f5b1848920
Stephen Poole <Stephen.Poole@redlioncanada.com>
2015-11-30 14:01:03 -0500

    Results features now rank according to their score. Fixed sliders not affecting appliance scoring.

--- app/js/slider-buttons.js
+++ app/js/slider-buttons.js
@@ -8,7 +8,7 @@ angular.module('App')
       $rootScope.controls.questionHasAnswer = false;
       for (var t in $rootScope.questionsData.question.text) {
         for (var i in $rootScope.questionsData.question.text[t].answers) {
-          if ($rootScope.questionsData.question.text[t].answers[i].value == $rootScope.questionsData.question.text[t].answer) {
+          if ($rootScope.questionsData.question.text[t].answers[i].value == Math.round($rootScope.questionsData.question.show.answer)) {
             $rootScope.questionsData.question.text[t].answers[i].answer = true
             if (t == 1) $rootScope.controls.questionHasAnswer = true
           } else {
